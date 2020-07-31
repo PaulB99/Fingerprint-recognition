75df00ce0beb21627ff5754c81d3b6484e8856b8
pab734 <pab734@cs.bham.ac.uk>
2020-07-31 23:18:04 +0100

    Compiles

--- src/PrePro.c
+++ src/PrePro.c
@@ -5,6 +5,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * A function to relate the neighbour positions
+ */
+int findpos (int t[]) {
+	int x;
+	x = t[0] + (3 * t[1]); // Add both values and multiply the second by 3, producing unique values
+	switch(x) {
+		case -3 : // 0,1
+			return 1;
+    case -2 : // 1,-1
+	return 2;
+    case 1 : // 1,0
+	return 3;
+    case 4 : // 1,1
+	return 4;
+    case 3 : // 0,1
+	return 5;
+    case 2 :// -1,1
+	return 6;
+    case -1 :// -1,0
+	return 7;
+    case -4 :// -1,-1
+	return 8;
+	}
+}
+
+/** 
+* Function to straighten an image
+*/
 void straighten(char path[]) {
 
     unsigned char byte[54];
@@ -108,45 +137,36 @@ void skeleton(char path[]) {
 /** 
  * Zhang-Suen skeletonisation algorithm
  */
-char skeletonise(char img[], int height, int width) {
-    int size = height * weight;
+/**char skeletonise(char img[], int height, int width) {
+    int size = height * width;
     int i, j;
     for(i = 0; i > )
-}
+
+} */
 
 /** 
  * Returns the neighbours of a cell, from the cell directly above and clockwise around
  */
 char neighbours(char img[], int height, int width, int pos) {
-    char neigh[8]
+    char neigh[8];
     int i, j;
     int size = height * width;
     for (i = -1; i <= 1; i++) {
         for (j = -1; j <= 1; j++) {
+	int arr[] = {i, j};
             if (i == 0 && j == 0) { // Skip central case
                 continue;
             }
             else if(pos-i<0 || pos+i>size-1 || pos+(j*width)>size-1 || pos-(j*width)<0) {
-                neigh[findpos(i, j)] = NULL;
+                neigh[findpos(arr)] = NULL;
             }
             else {
-                neigh[findpos(i, j)] = pos + i + (j*width)
+                neigh[findpos(arr)] = pos + i + (j*width);
             }
 
         }
     } 
+	return neigh;
 }
 
-/**
- * An enum to relate the neighbour positions
- */
-enum findpos {
-    (0, -1) = 1
-    (1, -1) = 2
-    (1, 0) = 3
-    (1, 1) = 4
-    (0, 1) = 5
-    (-1, 1) = 6
-    (-1, 0) = 7
-    (-1, -1) = 8
-};
+
