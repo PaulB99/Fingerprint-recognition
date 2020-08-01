076f086f0818adbf4d2f875728bc686e319d4ff6
Paul <paulbyrne99@rocketmail.com>
2020-08-01 19:59:29 +0100

    different approach to importing

--- src/PrePro.c
+++ src/PrePro.c
@@ -85,56 +85,32 @@ void straighten(char path[]) {
 /** 
  * Zhang-Suen skeletonisation algorithm
  */
-void skeleton(char path[]) {
+void skeleton(char* path) {
 
-    unsigned char byte[54];
-    int i;
+    // Declare variables
+    int startRow = 1,startCol = 1,endRow,endCol,i,j,count,rows,cols,processed;
+ 
+	pixel* markers;
 
-    // Input file
-    FILE *fIn = fopen(path ,"r");
-    // Output file				            
-	FILE *fOut = fopen("out.bmp","w+");                         
-
-    // Check if the input file has not been opened succesfully
-    if(fIn==NULL)												
-	{											
-		printf("File does not exist \n");
+    // Import file
+    FILE* in = fopen(path,"r");
+	fscanf(in,"%d%d",&rows,&cols);
+	fscanf(in,"%d%d",&blankPixel,&imagePixel);
+ 
+	blankPixel<=9?blankPixel+='0':blankPixel;
+	imagePixel<=9?imagePixel+='0':imagePixel;
+ 
+	printf("\nPrinting original image :\n");
+ 
+	image = (char**)malloc(rows*sizeof(char*));
+ 
+	for(i=0;i<rows;i++){
+		image[i] = (char*)malloc((cols+1)*sizeof(char));
+		fscanf(in,"%s\n",imageMatrix[i]);
+		printf("\n%s",image[i]);
+ 
 	}
-
-    // 	Copy over the file header
-    for(i=0;i<54;i++)										    
-	{									
-		byte[i] = getc(fIn);								
-	}
-	fwrite(byte,sizeof(unsigned char),54,fOut);	
-
-    // extract image height, width and bitDepth from the header 
-	int height = *(int*)&byte[18];
-	int width = *(int*)&byte[22];
-	int bitDepth = *(int*)&byte[28];
-
-    // Find size of image and fill a buffer with the image
-    int size = height*width;
-	unsigned char buffer[size],out[size];	
-    fread(buffer,sizeof(unsigned char),size,fIn);
-
-    // Copy to out buffer
-    for(i=0;i<size;i++)
-	{
-		out[i] = buffer[i];
-	}
-
-    // ALGORITHM HERE
-	for (i=0; i<size; i++) 
-	{ 
-		printf("%c", out[i];
-	}
-
-    //write image data back to the file
-    fwrite(out,sizeof(unsigned char),size,fOut);
-
-	fclose(fIn);
-	fclose(fOut);
+ 
 }
 
 /**
